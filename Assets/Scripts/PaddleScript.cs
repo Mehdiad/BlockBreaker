f2b39760172d48a4a2b90f5f78fb80d9aa02648a
mehdi <mehdiadelzadeh95@gmail.com>
2020-01-08 23:14:42 +0100

    added mouse and paddle connection

---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+
+    [SerializeField]
+    float screenWidth = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosX = (Input.mousePosition.x / Screen.width * screenWidth);
+        Vector2 paddlePos = new Vector2(mousePosX, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
