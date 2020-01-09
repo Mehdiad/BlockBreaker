0e2b4b8c631dbe0e4ae13248d09f34e5dc74d31f
mehdi <mehdiadelzadeh95@gmail.com>
2020-01-09 16:19:23 +0100

    added ball movement

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+
+    [SerializeField]
+    PaddleScript paddle;
+
+    [SerializeField]
+    float launchPosX = 2f;
+
+    [SerializeField]
+    float launchPosY = 15f;
+
+
+
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasStarted == false)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    //left button
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(launchPosX, launchPosY);
+            hasStarted = true;
+        }
+    }
+
+}
