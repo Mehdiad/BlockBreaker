1406ad2347c5d5958ae4dd3d23b93b800b737ff5
mehdi <mehdiadelzadeh95@gmail.com>
2020-01-09 16:19:06 +0100

    add destroy method for block

---
+++ Assets/Scripts/BlockScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log(gameObject.name);
+        Destroy(gameObject);
+        //Debug.Log(collision.gameObject.name);
+    }
+
+}
